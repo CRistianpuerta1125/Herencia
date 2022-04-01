9dafdf1d75ffd84ad1235e85df65e0276c690463
cristiany <crisdam1840@gmail.com>
2022-04-01 12:46:16 -0500

    herencia

--- src/test/TestHerencia.java
+++ src/test/TestHerencia.java
@@ -6,8 +6,8 @@ import java.sql.Date;
 
 public class TestHerencia {
     public static void main(String[] args) {
-        //Empleado empleado1 = new Empleado("Juan", 5000.00);
-        //System.out.println("empleado1 = " + empleado1);
+        Empleado empleado1 = new Empleado("Juan", 5000.00);
+        System.out.println("empleado1 = " + empleado1);
         var fecha = new Date(2021-02-01);
         Cliente cliente1 = new Cliente(fecha, true, "karla", 'F', 28, "Saturno 15");
         System.out.println("cliente1 = " + cliente1);
